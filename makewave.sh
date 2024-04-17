9fd3637d23a32f43b4367a564b160049a67744a8
WhatACotton <whatacotton@gmail.com>
2024-04-17 14:49:32 +0900

    01 02

--- makewave.sh
+++ makewave.sh
@@ -4,4 +4,4 @@ filename_without_extension="${filename%.*}"  # 拡張子を削除
 
 iverilog $1 -o $filename_without_extension.out
 vvp $filename_without_extension.out
-gtkwave wave.vcd
+gtkwave *.vcd
---
+++ questions/01-CRUDE-AWAKENING/impl.v
@@ -0,0 +1,30 @@
+module CRUDE_AWAKENING(
+     input wire       in0,
+     output wire       out
+);
+    assign out = in0;
+endmodule
+
+module CRUDE_AWAKENING_tb;
+
+    reg in0 = 1'b0;
+
+    initial begin
+        $dumpfile("crude_awakening.vcd");
+        $dumpvars(0, DUT);
+    end
+
+    CRUDE_AWAKENING DUT(
+        .in0    (in0    ),
+        .out    (out    )
+    );
+
+    initial begin
+        in0 = 1'b0;
+        #2
+        in0 = 1'b1;
+        #2
+        $finish;
+    end
+
+endmodule
