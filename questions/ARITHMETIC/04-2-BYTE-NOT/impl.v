2a7cdeb84342746e3266780397261973288fda90
WhatACotton <whatacotton@gmail.com>
2024-04-21 07:03:52 +0900

    04-1 04-2

--- questions/ARITHMETIC/04-2-BYTE-NOT/impl.v
+++ questions/ARITHMETIC/04-2-BYTE-NOT/impl.v
@@ -1,53 +1,39 @@
-module BYTE_OR(
+module BYTE_NOT(
      input wire [7:0] in0,
-     input wire [7:0] in1,
      output wire [7:0] out
 );
-OR_GATE OR_0(
+NOT_GATE NOT_0(
         .in0    (in0[0]    ),
-        .in1    (in1[0]    ),
         .out    (out[0]    )
         );
-       
-OR_GATE OR_1(
+NOT_GATE NOT_1(
         .in0    (in0[1]    ),
-        .in1    (in1[1]    ),
         .out    (out[1]    )
         );
-
-OR_GATE OR_2(
+NOT_GATE NOT_2(
         .in0    (in0[2]    ),
-        .in1    (in1[2]    ),
         .out    (out[2]    )
         );
-
-OR_GATE OR_3(
+NOT_GATE NOT_3(
         .in0    (in0[3]    ),
-        .in1    (in1[3]    ),
         .out    (out[3]    )
         );
-
-OR_GATE OR_4(
+NOT_GATE NOT_4(
         .in0    (in0[4]    ),
-        .in1    (in1[4]    ),
         .out    (out[4]    )
         );
-        
-OR_GATE OR_5(
+NOT_GATE NOT_5(
         .in0    (in0[5]    ),
-        .in1    (in1[5]    ),
         .out    (out[5]    )
         );
-   
-OR_GATE OR_6(
+NOT_GATE NOT_6(
         .in0    (in0[6]    ),
-        .in1    (in1[6]    ),
         .out    (out[6]    )
         );
-
-OR_GATE OR_7(
+NOT_GATE NOT_7(
         .in0    (in0[7]    ),
-        .in1    (in1[7]    ),
         .out    (out[7]    )
         );
+
+   
 endmodule
--- questions/ARITHMETIC/04-2-BYTE-NOT/tb.v
+++ questions/ARITHMETIC/04-2-BYTE-NOT/tb.v
@@ -1,6 +1,5 @@
-module BYTE_OR_tb;
+module BYTE_NOT_tb;
     reg    [7:0]    in0 = 8'h00;
-    reg    [7:0]    in1 = 8'h00;
     wire   [7:0]    out;
     
 initial begin
@@ -9,28 +8,18 @@ initial begin
     end
     
 
-    BYTE_OR DUT(
+    BYTE_NOT DUT(
         .in0    (in0    ),
-        .in1    (in1    ),
         .out    (out    )
     );
     
     initial begin
         #2
         in0 = 8'h01;
-        in1 = 8'h01;
-        #2
-        in0 = 8'h01;
-        in1 = 8'h02;
         #2
         in0 = 8'h05;
-        in1 = 8'h10;
-        #2
-        in0 = 8'h08;
-        in1 = 8'h08;
         #2
         in0 = 8'hff;
-        in1 = 8'hff;
         #2
         $finish;
     end
